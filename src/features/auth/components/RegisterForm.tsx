255212e0efba2fe86de20328d57f82f2a8158030
jhonatan182 <jona.vand98@gmail.com>
2026-03-15 12:59:07 -0600

    feat: react-hook-form and zod

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,29 +1,70 @@
 "use client";
 
-import { Form, FormInput, FormLabel, FormSubmit } from "@/components/forms";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormError,
+  FormInput,
+  FormLabel,
+  FormSubmit,
+} from "@/components/forms";
+import { SignUpSchema, SignUpInput } from "../schemas/authSchema";
 
 export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(SignUpSchema),
+    mode: "all",
+  });
+
+  const onSubmit = (data: SignUpInput) => {
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <FormLabel htmlFor="name">Nombre</FormLabel>
-      <FormInput type="text" id="name" placeholder="Ingresa tu nombre" />
+      <FormInput
+        type="text"
+        id="name"
+        placeholder="Ingresa tu nombre"
+        {...register("name")}
+      />
+      {errors.name && <FormError>{errors.name.message}</FormError>}
 
       <FormLabel htmlFor="email">E-mail</FormLabel>
-      <FormInput type="email" id="email" placeholder="Ingresa tu e-mail" />
+      <FormInput
+        type="email"
+        id="email"
+        placeholder="Ingresa tu e-mail"
+        {...register("email")}
+      />
+      {errors.email && <FormError>{errors.email.message}</FormError>}
 
       <FormLabel htmlFor="password">Contraseña</FormLabel>
       <FormInput
         type="password"
         id="password"
-        placeholder="Ingresa tu contraseña "
+        placeholder="Ingresa tu contraseña"
+        {...register("password")}
       />
+      {errors.password && <FormError>{errors.password.message}</FormError>}
 
       <FormLabel htmlFor="confirmPassword">Confirmar contraseña</FormLabel>
       <FormInput
         type="password"
         id="confirmPassword"
         placeholder="Ingresa tu contraseña nuevamente"
+        {...register("passwordConfirmation")}
       />
+      {errors.passwordConfirmation && (
+        <FormError>{errors.passwordConfirmation.message}</FormError>
+      )}
 
       <FormSubmit value="Registrarme" />
     </Form>
